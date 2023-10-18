de0ec5b298c7cdae257031eb3ea8f7d6993bd949
wechiley1 <wechiley@live.com>
2023-10-18 13:27:42 -0300

    correcao de codigo

--- sca/aplic/admin.py
+++ sca/aplic/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from .models import Cliente, Pagamento, OrdemDeServico, Recibo, Funcionario, Cargo, Peca, Veiculo, Carro, Moto, Caminhao
 
-# Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Pagamento)
+admin.site.register(OrdemDeServico)
+admin.site.register(Recibo)
+admin.site.register(Funcionario)
+admin.site.register(Cargo)
+admin.site.register(Peca)
+admin.site.register(Veiculo)
+admin.site.register(Carro)
+admin.site.register(Moto)
+admin.site.register(Caminhao)
+
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'sobrenome', 'cpf', 'email', 'telefone1']
+    list_filter = ['cidade', 'estado']
+    search_fields = ['nome', 'sobrenome', 'cpf', 'email']
+
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'valor final servico', 'status pagamento']
+    list_filter = ['status pagamento']
+    search_fields = ['cliente__nome', 'cliente__sobrenome', 'cliente__cpf']
+
+
+class OrdemDeServicoAdmin(admin.ModelAdmin):
+    list_display = ['pagamento', 'status servico', 'dt_hora_entrada', 'dt_hora_saida']
+    list_filter = ['status servico']
+    search_fields = ['pagamento__cliente__nome', 'pagamento__cliente__sobrenome']
+
+
+class ReciboAdmin(admin.ModelAdmin):
+    list_display = ['ordem de servico', 'valorecibo', 'datahoraemissao']
+    list_filter = ['ordem de servico__status servico']
+    search_fields = ['ordem de servico__pagamento__cliente__nome', 'ordem de servico__pagamento__cliente__sobrenome']
+
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome funcionario', 'cargo funcionario']
+    list_filter = ['cargo funcionario']
+    search_fields = ['nome funcionario']
+
+
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['nome cargo']
+
+class PecaAdmin(admin.ModelAdmin):
+    list_display = ['Tipo Peca', 'Marca Peca', 'Modelo Peca', 'Estoque Atual', 'Preco Compra', 'Preco Venda Final']
+    list_filter = ['Tipo Peca', 'Marca Peca']
+    search_fields = ['Tipo Peca', 'Marca Peca', 'Modelo Peca']
+    ordering = ['Tipo Peca']
+
+class VeiculoAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'Tipo Veiculo', 'Placa Veiculo', 'Ano Veiculo']
+    list_filter = ['Tipo Veiculo', 'Placa Veiculo']
+    search_fields = ['Tipo Veiculo', 'Placa Veiculo']
+
+
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ['Tipo Veiculo', 'Placa Veiculo', 'Ano Veiculo', 'cavalos']
+
+
+class MotoAdmin(admin.ModelAdmin):
+    list_display = ['Tipo Veiculo', 'Placa Veiculo', 'Ano Veiculo', 'cilindrada']
+
+
+
+class CaminhaoAdmin(admin.ModelAdmin):
+    list_display = ['Tipo Veiculo', 'Placa Veiculo', 'AnoVeiculo', 'capacidade']
+
+
+
+
+
--- sca/aplic/models.py
+++ sca/aplic/models.py
@@ -1,3 +1,126 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    rg = models.CharField(max_length=8)
+    cnpj = models.CharField(max_length=14)
+    razaosocial = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+    cep = models.CharField(max_length=8)
+    telefone1 = models.CharField(max_length=15)
+    telefone2 = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
+class Pagamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    valorfinalservico = models.DecimalField(max_digits=10, decimal_places=2)
+    valorrecebido = models.DecimalField(max_digits=10, decimal_places=2)
+    restante = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    anodereferencia = models.PositiveIntegerField()
+    totalpago = models.BooleanField(default=False)
+    dthorapagamento = models.DateTimeField()
+    formapagamento = models.CharField(max_length=50)
+    STATUS_PAGAMENTO_CHOICES = [
+        ('Pendente', 'Pendente'),
+        ('Pago', 'Pago')
+    ]
+    statuspagamento = models.CharField(max_length=10, choices=STATUS_PAGAMENTO_CHOICES)
+
+    def save(self, *args, **kwargs):
+        self.restante = self.valorfinalservico - self.valorrecebido
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Pagamento de {self.cliente}"
+
+class OrdemDeServico(models.Model):
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE, null=True, blank=True)
+    defeitoreclamado = models.TextField()
+    procedimentorealizado = models.TextField()
+    dt_hora_entrada = models.DateTimeField()
+    dt_hora_saida = models.DateTimeField(null=True, blank=True)
+    valorservico = models.DecimalField(max_digits=10, decimal_places=2)
+    STATUS_SERVICO_CHOICES = [
+        ('PARA FAZER', 'Para Fazer'),
+        ('EM ANDAMENTO', 'Em Andamento'),
+        ('FINALIZADO', 'Finalizado'),
+        ('ENTREGUE', 'Entregue')
+    ]
+    statusservico = models.CharField(max_length=15, choices=STATUS_SERVICO_CHOICES)
+
+    def __str__(self):
+        return f"Ordem de Serviço #{self.id}"
+
+class Recibo(models.Model):
+    valorecibo = models.DecimalField(max_digits=10, decimal_places=2)
+    datahoraemissao = models.DateTimeField()
+    ordensdeservico = models.ManyToManyField(OrdemDeServico)
+
+    def __str__(self):
+        return f"Recibo #{self.id}"
+
+class Funcionario(models.Model):
+    nomefuncionario = models.CharField(max_length=100)
+    cargofuncionario = models.CharField(max_length=50)
+    ordensdeservico = models.ManyToManyField(OrdemDeServico)
+
+    def __str__(self):
+        return f"{self.nomefuncionario} ({self.cargofuncionario})"
+
+class Cargo(models.Model):
+    nomecargo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomecargo
+
+class Peca(models.Model):
+    TipoPeca = models.CharField(max_length=100)
+    MarcaPeca = models.CharField(max_length=100)
+    ModeloPeca = models.CharField(max_length=100)
+    EstoqueAtual = models.PositiveIntegerField()
+    PrecoCompra = models.DecimalField(max_digits=10, decimal_places=2)
+    PrecoVendaFinal = models.DecimalField(max_digits=10, decimal_places=2)
+    QtdTotalVendida = models.PositiveIntegerField()
+    TotalEmVendas = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.TipoPeca} ({self.MarcaPeca})"
+
+class Veiculo(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    TipoVeiculo = models.CharField(max_length=100)
+    PlacaVeiculo = models.CharField(max_length=20)
+    AnoVeiculo = models.PositiveIntegerField()
+    ModeloVeiculo = models.CharField(max_length=100)
+    Potencia = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.TipoVeiculo} - {self.PlacaVeiculo}"
+
+class Carro(Veiculo):
+    cavalos = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Carro - {self.PlacaVeiculo}"
+
+class Moto(Veiculo):
+    cilindrada = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Moto - {self.PlacaVeiculo}"
+
+class Caminhao(Veiculo):
+    capacidade = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Caminhão - {self.PlacaVeiculo}"
+
